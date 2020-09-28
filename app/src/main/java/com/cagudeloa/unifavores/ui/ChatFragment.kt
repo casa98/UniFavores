4e4d173e4e114f84e2b4bd7f22f47f4ee31ead5d
casa98 <cagudeloa@uninorte.edu.co>
2020-09-28 10:15:29 -0500

    Send messages (but not shown yet)

--- app/src/main/java/com/cagudeloa/unifavores/ui/ChatFragment.kt
+++ app/src/main/java/com/cagudeloa/unifavores/ui/ChatFragment.kt
@@ -1,4 +1,4 @@
- package com.cagudeloa.unifavores.ui
+package com.cagudeloa.unifavores.ui
 
 import android.os.Bundle
 import android.util.Log
@@ -15,40 +15,58 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 
- class ChatFragment : Fragment() {
+class ChatFragment : Fragment() {
 
-    private lateinit var userId: String
-    private lateinit var binding: FragmentChatBinding
-    private var firebaseUser: FirebaseUser? = null
-    private var databaseReference: DatabaseReference? = null
+private lateinit var userId: String
+private lateinit var binding: FragmentChatBinding
+private var firebaseUser: FirebaseUser? = null
+private var databaseReference: DatabaseReference? = null
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         requireArguments().let {
-            userId = it.getString("user")!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireArguments().let {
+        userId = it.getString("user")!!
+        }
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+        databaseReference!!.addValueEventListener(object : ValueEventListener{
+         override fun onDataChange(snapshot: DataSnapshot) {
+            val user = snapshot.getValue(User::class.java)
+             (activity as AppCompatActivity).supportActionBar?.title = user!!.username
          }
-         firebaseUser = FirebaseAuth.getInstance().currentUser
-         databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
-         databaseReference!!.addValueEventListener(object : ValueEventListener{
-             override fun onDataChange(snapshot: DataSnapshot) {
-                val user = snapshot.getValue(User::class.java)
-                 (activity as AppCompatActivity).supportActionBar?.title = user!!.username
-             }
 
-             override fun onCancelled(error: DatabaseError) {
+         override fun onCancelled(error: DatabaseError) {
 
-             }
+         }
 
-         })
-     }
+        })
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         Log.i("UID: ", userId)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false)
-
-
-
         return binding.root
     }
- }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.sendMessageButton.setOnClickListener{
+            val message = binding.messageEdit.text.toString()
+            if(message.isNotEmpty()){
+                sendMessage(firebaseUser!!.uid, userId, message)
+            }
+        }
+    }
+
+    private fun sendMessage(senderId: String, receiverId: String, message: String){
+        val databaseReference: DatabaseReference? = FirebaseDatabase.getInstance().getReference()
+
+        val hashMap: HashMap<String, String> = HashMap()
+        hashMap["senderId"] = senderId
+        hashMap["receiverId"] = receiverId
+        hashMap["message"] = message
+        databaseReference!!.child("Chat").push().setValue(hashMap)
+    }
+
+}
