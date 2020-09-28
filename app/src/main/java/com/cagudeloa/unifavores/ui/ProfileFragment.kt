0be7cf0df18c7387a6fa6747dcc6e823d768dc02
casa98 <cagudeloa@uninorte.edu.co>
2020-09-28 01:53:19 -0500

    Half implement Profile fragment

--- app/src/main/java/com/cagudeloa/unifavores/ui/ProfileFragment.kt
+++ app/src/main/java/com/cagudeloa/unifavores/ui/ProfileFragment.kt
@@ -5,12 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.cagudeloa.unifavores.R
+import com.cagudeloa.unifavores.databinding.FragmentProfileBinding
+import com.cagudeloa.unifavores.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var firebaseUser: FirebaseUser
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser!!
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.uid)
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                username_profile_text.text = user!!.username
+                if(user.image!=""){
+                    // TODO Bring image using Picasso or Glide, etc
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+        return binding.root
     }
+
 }
