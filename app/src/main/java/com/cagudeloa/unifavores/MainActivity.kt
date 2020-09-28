db6231f4b025d8836ae51efc92b42143585417b1
casa98 <cagudeloa@uninorte.edu.co>
2020-09-28 01:25:10 -0500

    App support for Up Button. Delete unsuded fragments

--- app/src/main/java/com/cagudeloa/unifavores/MainActivity.kt
+++ app/src/main/java/com/cagudeloa/unifavores/MainActivity.kt
@@ -3,15 +3,27 @@ package com.cagudeloa.unifavores
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
