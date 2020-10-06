494af42b98e6ceef9e97b4de84b615264471d8c9
casa98 <cagudeloa@uninorte.edu.co>
2020-10-06 16:18:25 -0500

    Show assigned favors in Menu option

---
+++ app/src/main/java/com/cagudeloa/unifavores/ui/incompleteFavors/IncompleteFavorsAdapter.kt
@@ -0,0 +1,39 @@
+package com.cagudeloa.unifavores.ui.incompleteFavors
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cagudeloa.unifavores.R
+import com.cagudeloa.unifavores.base.BaseViewHolder
+import com.cagudeloa.unifavores.model.Favor
+import kotlinx.android.synthetic.main.incomplete_favor_item.view.*
+
+class IncompleteFavorsAdapter(
+    private val context: Context,
+    //private val itemClickListener: FavorsAdapter.OnItemClickListener,
+    private val favor: ArrayList<Favor>
+): RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    inner class FavorsViewHolder(itemView: View): BaseViewHolder<Favor>(itemView){
+        override fun bind(item: Favor, position: Int) {
+            itemView.favorTitleText.text = item.favorTitle
+            itemView.favorDescriptionText.text = item.favorDescription
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        return FavorsViewHolder(LayoutInflater.from(context).inflate(R.layout.incomplete_favor_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        if(holder is FavorsViewHolder)
+            holder.bind(favor[position], position)
+    }
+
+    override fun getItemCount(): Int {
+        return favor.size
+    }
+}
