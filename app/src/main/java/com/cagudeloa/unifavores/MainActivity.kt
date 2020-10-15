5c79056d1023125ed616c8972ae9fa68ca46142d
casa98 <cagudeloa@uninorte.edu.co>
2020-10-15 01:24:40 -0500

    Set username and email on drawer navheader

--- app/src/main/java/com/cagudeloa/unifavores/MainActivity.kt
+++ app/src/main/java/com/cagudeloa/unifavores/MainActivity.kt
@@ -13,6 +13,12 @@ import androidx.navigation.ui.setupWithNavController
 import com.cagudeloa.unifavores.auth.LoginActivity
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.header_layout.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,9 +33,34 @@ class MainActivity : AppCompatActivity() {
             setSupportActionBar(toolbar)
             val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
             val navView: NavigationView = findViewById(R.id.nav_view)
+
+            //TODO find the way to do it using some Architecture (OHHH, USING LIVEDATA AND DATABINBING DIRECTLY)
+            val headerView = navView.getHeaderView(0)
+            // Get user name and image (someday, why not) from model. Email from Firebase.
+            val currentUser = FirebaseAuth.getInstance().currentUser!!
+            headerView.email_header.text = currentUser.email
+            FirebaseDatabase.getInstance().getReference(NODE_USERS).child(currentUser.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val username = snapshot.child("username").value.toString()
+                        headerView.name_header.text = username
+                        val imageURL =
+                            snapshot.child("image").value.toString()    // Load with Picasso
+                        if(imageURL.isNotEmpty()){
+                            Picasso.get().load(imageURL).placeholder(R.drawable.someone)
+                                .error(R.drawable.someone).into(headerView.image_header)
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {}
+                })
+
+
             val navController = findNavController(R.id.nav_host_fragment)
             appBarConfiguration = AppBarConfiguration(
-                setOf(
+                // I only want a top-level destination, so I pass navController.graph instead of fragments ID
+                navController.graph, drawerLayout
+                /*setOf(
                     R.id.nav_home,
                     R.id.profileFragment,
                     R.id.myFavorsFragment,
@@ -37,6 +68,7 @@ class MainActivity : AppCompatActivity() {
                     R.id.chatsFragment,
                     R.id.statisticsFragment
                 ), drawerLayout
+                */
             )
             setupActionBarWithNavController(navController, appBarConfiguration)
             navView.setupWithNavController(navController)
